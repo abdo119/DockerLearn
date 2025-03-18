f5fee9d162edd77e7a5858d7f41236c8235a84f2
Mahmoud Ahmed <mahmoudahmed@Mahmouds-MacBook-Air.local>
2025-03-18 23:53:38 +0200

    make docker file and build it on docker

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 // init app
 const PORT = 4000;
 const app = express();
-app.get ('/', (req, res) => res.send ('<h1> Hello Tresme!</h1>'))
+app.get ('/', (req, res) => res.send ('<h1> Hello Tresme!Hiiiiì</h1>'))
 app.listen(PORT, () => console.log(`app is up and running on port:${PORT}`))
