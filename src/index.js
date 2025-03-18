512cef6b26b5d5b706f43036ab9b8747e165e90f
Mahmoud Ahmed <mahmoudahmed@Mahmouds-MacBook-Air.local>
2025-03-19 00:30:42 +0200

    add ro binding and use docker compose

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const express = require('express');
 // init app
 const PORT = 4000;
 const app = express();
-app.get ('/', (req, res) => res.send ('<h1> Hello Tresme!Hiiiiì</h1>'))
+app.get ('/', (req, res) => res.send ('<h1> Hello Tresme!Hiiiddddssssssiì</h1>'))
 app.listen(PORT, () => console.log(`app is up and running on port:${PORT}`))
